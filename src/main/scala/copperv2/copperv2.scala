d4fb244ea63ee59ac70c0b4ecf562aedcf314abf
diegob94 <dbim.bena@gmail.com>
2022-01-24 23:48:08 -0300

    Start working on wishbone xbar

--- src/main/scala/copperv2/copperv2.scala
+++ src/main/scala/copperv2/copperv2.scala
@@ -2,7 +2,6 @@ package copperv2
 
 import chisel3._
 import chisel3.util.{Decoupled,MuxLookup,Cat}
-import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 import chisel3.internal.requireIsChiselType
 import chisel3.experimental.dataview._
 import chisel3.util.DecoupledIO
@@ -263,10 +262,3 @@ class Copperv2Core(config: Copperv2Config = new Copperv2Config()) extends Module
   cpu.bus <> bus
 }
 
-object Copperv2Driver extends App {
-  val verilog_args = Array("--target-dir", "work/rtl") ++ args
-  (new ChiselStage).emitVerilog(new copperv2.Copperv2, verilog_args ++ Array("-o","copperv2.v"))
-//  (new ChiselStage).execute(Array("--emit-modules", "verilog"),Seq(ChiselGeneratorAnnotation(() => new copperv2.copperv2)))
-  (new ChiselStage).emitVerilog(new wishbone.WishboneAdapter(32,32,1), verilog_args ++ Array("-o","wb_adapter.v"))
-  (new ChiselStage).emitVerilog(new copperv2.CoppervSoC, verilog_args ++ Array("-o","copperv_soc_top.v"))
-}
---
+++ src/main/scala/main.scala
@@ -0,0 +1,11 @@
+
+import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
+
+object Copperv2Driver extends App {
+  val verilog_args = Array("--target-dir", "work/rtl") ++ args
+  (new ChiselStage).emitVerilog(new copperv2.Copperv2, verilog_args ++ Array("-o","copperv2.v"))
+//  (new ChiselStage).execute(Array("--emit-modules", "verilog"),Seq(ChiselGeneratorAnnotation(() => new copperv2.copperv2)))
+  (new ChiselStage).emitVerilog(new wishbone.WishboneAdapter(32,32,1), verilog_args ++ Array("-o","wb_adapter.v"))
+//  (new ChiselStage).emitVerilog(new copperv2.CoppervSoC, verilog_args ++ Array("-o","copperv_soc_top.v"))
+  (new ChiselStage).emitVerilog(new wishbone.CrossBar(2), verilog_args ++ Array("-o","crossbar.v"))
+}
